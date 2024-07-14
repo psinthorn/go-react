96af0cd0d66f61942749c3502173e1ab5758d386
F2x <f2coltd@F2s-iMac.local>
2024-07-15 00:08:45 +0700

    folder structure

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="tex-center">Select a movies to watch </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;

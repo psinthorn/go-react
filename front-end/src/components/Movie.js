0d1cf9990c14cdf4bbca25e876fb1e1c4c3cd448
F2x <f2coltd@F2s-iMac.local>
2024-07-17 23:37:39 +0700

    implement movie module

--- front-end/src/components/Movie.js
+++ front-end/src/components/Movie.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
 
-const Movie = ({ title, mpaa_rate, score}) => {
+const Movie = ({ title, id, mpaa_rate, score}) => {
+  const [movie, setMovie] = useState({});
+  // const id = useParams({id});
   return (
     <div>
-      <p className='text-center'>{title}</p>
+      <p className='text-center'><Link to={`/movies/${id}`}>{title}</Link></p>
         <div className='row'>
         <div className='col'>{mpaa_rate}</div>
         <div className='col'>{score}</div>

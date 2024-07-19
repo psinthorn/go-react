ede677c692d89ef4c057a6ec7e94961c17d414a6
F2x <f2coltd@F2s-iMac.local>
2024-07-19 10:54:50 +0700

    implement login function and form

--- front-end/src/components/Movie.js
+++ front-end/src/components/Movie.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 
 const Movie = ({ title, mpaa_rate, score}) => {
   const [movie, setMovie] = useState({});
-  const movieId = useParams();
+  const { id } = useParams();
+
+  useEffect(() => {
+    let myMovie = {
+      id: 1,
+      title: "Black Hawk Down",
+      release_date: "11-12-2023",
+      runtime: "148",
+      mpaa_rating: "18+",
+      description: "Operation on Mogadishu",
+      status: true,
+    }
+
+    setMovie(myMovie);
+
+  }, [id]);
+
   return (
     <div>
-      <p className='text-center'><Link to={`/movies/${movieId}`}>{title}</Link></p>
+      <h2 className='text-center'><Link to={`/movie/${movie.id}`}>{movie.title}</Link></h2>
+      <small><em>{movie.release_date}, {movie.runtime} minutes, Rated: {movie.mpaa_rating}</em></small>
+      <hr/>
+      <p>
+        {movie.description}
+      </p>
         <div className='row'>
         <div className='col'>{mpaa_rate}</div>
         <div className='col'>{score}</div>
